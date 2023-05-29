99b543acf17edfcdb6f9ba333c8d0e94bc7ebef9
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-05-30 02:38:40 +0300

    Implemented URL mapping in API

--- programming-api/app.js
+++ programming-api/app.js
@@ -2,7 +2,15 @@ import * as programmingAssignmentService from "./services/programmingAssignmentS
 import { serve } from "./deps.js";
 import { sql } from "./database/database.js";
 
-const handleRequest = async (request) => {
+const handleGetRoot = async (request) => {
+  return new Response(`Hello from ...`); //show user uuid?
+};
+
+const handleGetTest = async (request) => {
+  return new Response("Test message");
+};
+
+const handlePostGrade = async (request) => {
   const programmingAssignments = await programmingAssignmentService.findAll();
 
   const requestData = await request.json();
@@ -21,6 +29,39 @@ const handleRequest = async (request) => {
   });
 
   return response;
+};
+
+const urlMapping = [
+  {
+    method: "GET",
+    pattern: new URLPattern({ pathname: "/test" }),
+    fn: handleGetTest,
+  },
+  {
+    method: "GET",
+    pattern: new URLPattern({ pathname: "/" }),
+    fn: handleGetRoot,
+  },
+  {
+    method: "POST",
+    pattern: new URLPattern({ pathname: "/grade" }),
+    fn: handlePostGrade,
+  },
+];
+
+const handleRequest = async (request) => {
+  const mapping = urlMapping.find(
+    (um) => um.method === request.method && um.pattern.test(request.url)
+  );
+
+  if (!mapping) {
+    console.log("Calling mapping Result because of URL ", request.url);
+    return new Response("Not found", { status: 404 });
+  }
+
+  const mappingResult = mapping.pattern.exec(request.url);
+  console.log("Calling mapping Result because of URL ", request.url);
+  return await mapping.fn(request, mappingResult);
 };
 
 const portConfig = { port: 7777, hostname: "0.0.0.0" };
