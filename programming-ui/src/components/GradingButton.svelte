f57322bfc47bd9ed08ca4d65e7e296d2d26a36e3
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-06-02 13:34:22 +0300

    started to add submission success status bar

--- programming-ui/src/components/GradingButton.svelte
+++ programming-ui/src/components/GradingButton.svelte
@@ -2,6 +2,8 @@
   import { userUuid } from "../stores/stores.js";
   export let assignmentID = 1;
   let userCode = "";
+  let submissionSuccessful = false;
+  let submissionEvent = false;
   
   const submitAssignmentCode = async () => {
 
@@ -30,16 +32,31 @@
     const jsonData = await response.json();
     console.log(jsonData);
     alert(JSON.stringify(jsonData));
+    //submissionSuccessful = true;
+    //TODO study grader api to see if succesful and adjust return
+    submissionEvent = true;
   };
 
   const getGrading = async () => {
 
   }
 </script>
-<textarea bind:value={userCode} class="w-full bg-gray-900 text-white font-mono p-2.5 h-48 border-4 border-black focus:border-4 focus:border-blue-500" placeholder="Write your Pyhton code here..."></textarea>
+<textarea bind:value={userCode} class="w-full bg-gray-900 text-white font-mono p-2.5 h-48 border-4 border-black focus:border-4 focus:border-blue-500" placeholder="Write your Python code here..."></textarea>
 <button
   class="bg-gray-600 hover:bg-gray-900 text-white font-bold p-4 m-4"
   on:click={submitAssignmentCode}
 >
   Submit for grading
 </button>
+{#if submissionSuccessful && submissionEvent}
+  <div class="bg-green-600">
+    <p>Your submission was successful!</p>
+    <p><a href="/assignment-2/">Go to the next assignment</a></p>
+  </div>
+{/if}
+{#if submissionSuccessful == false && submissionEvent}
+  <div class="bg-red-500">
+    <p>Your submission was not succesful!</p>
+    <p>to be error</p>
+  </div>
+{/if}
