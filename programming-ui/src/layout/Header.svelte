d652b4030b79c009898582436c8634a570a34c1d
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-06-03 07:57:13 +0300

    Sidebar design changes hover active

--- programming-ui/src/layout/Header.svelte
+++ programming-ui/src/layout/Header.svelte
@@ -4,7 +4,7 @@
 
 <nav class="p-4 mb-4 shadow">
   <div class="flow-root">
-    <span class="text-4xl text-gray-700 font-sans mb-2 float-left">Programming Assignments Website</span>
+    <span class="text-4xl text-gray-700 font-sans mb-2 float-left font-extrabold">Programming Assignments Website</span>
     <span class="font-sans text-s float-right">{$userUuid}</span>
   </div>
 </nav>
--- programming-ui/src/layout/Sidebar.svelte
+++
@@ -1,5 +0,0 @@
-<!-- TODO: Make sidebar dynamic -->
-<p>Test Sidebar Content</p>
-<p><a href="/">Assignment 1</a></p>
-<p><a href="/assignment-2/">Assignment 2</a></p>
-<p><a href="/assignment-3/">Assignment 3</a></p>
